97bcd45d185f84767dbc042373400d7a8e4ed423
jain <jain.j@armiasystems.com>
2018-07-24 12:28:48 +0530

    folder structure changed --clearing all files

--- models/files.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-var Schema = mongoose.Schema;
-var files = new Schema({"files_path":{"type":"String"},"mime_type":{"type":"String"},"module":{"type":"String"}},{collection:'files'});
-files.plugin(AutoIncrement,{inc_field: 'files_id'});
-module.exports = mongoose.model('files', files);
--- src/app/site/app.component.html
+++
@@ -1,1 +0,0 @@
-<h5>Hello World</h5>
