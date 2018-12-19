944d250ecc7401277e294e3238c6ad5094a4dc2e
jain <jain.j@armiasystems.com>
2018-12-19 19:05:51 +0530

    fixed untracked files

--- config/DB.js
+++
@@ -1,9 +0,0 @@
-// module.exports = {
-//    secret:'meansecure',
-//    DB: 'mongodb://localhost:27017/magpie_default'
-// };
-
-module.exports = {
-    secret:'meansecure',
-    DB: 'mongodb://magpieadmin:3wtBZDcI5e9c32g@127.0.0.1:27017/magpie?authMechanism=SCRAM-SHA-1'
- };
